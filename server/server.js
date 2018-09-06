0a34a17289981d90f40f867422e742f7bc7a35cb
yogaraja <yogaraja@yogaraja.lan>
2018-09-06 21:13:05 +0100

    initial commit

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://root:YOga7276@ds115625.mlab.com:15625/lyricaldb';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
